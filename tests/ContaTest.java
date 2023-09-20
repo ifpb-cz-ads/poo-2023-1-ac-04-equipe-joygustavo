c41ae0a33bbf0b305cd956a98ec618b9e80c4792
gustavo <gustavo.cesariosilva15@gmail.com>
2023-09-20 16:17:17 -0300

    Criando menu

--- tests/ContaTest.java
+++ tests/ContaTest.java
@@ -12,9 +12,31 @@ public class ContaTest{
         c.setNomeTitular(scanner.nextLine());
         System.out.print("Digite o numero da conta: ");
         c.setNumConta(scanner.nextLine());
-        scanner.close();
         System.out.println("Cliente registrado!");
         System.out.println("Nome: " + c.getNomeTitular());
         System.out.println("Numero Conta: " + c.getNumConta());
+        int opcao;
+        do {
+            System.out.println("1- Depositar 100\n" +
+                    "2- Sacar 100\n" +
+                    "0- Sair");
+            opcao = Integer.parseInt(scanner.nextLine());
+            switch (opcao) {
+                case 1:
+                    c.depositar(100);
+                    System.out.println(c.getSaldo());
+                    break;
+                case 2:
+                    c.sacar(100);
+                    System.out.println(c.getSaldo());
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    break;
+            }
+
+        } while(opcao != 0);
     }
 }
