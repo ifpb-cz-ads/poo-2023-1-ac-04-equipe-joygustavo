318eda1045a0fcd02fe1f1d87b69724038c96fad
gustavo <gustavo.cesariosilva15@gmail.com>
2023-09-27 08:53:02 -0300

    Criando classe Lampada.

---
+++ tests/LampadaTest.java
@@ -0,0 +1,10 @@
+package tests;
+
+import models.Lampada;
+
+public class LampadaTest {
+    public static void main(String[] args) {
+        Lampada l = new Lampada("15w","led", false);
+        System.out.println(l);
+    }
+}
